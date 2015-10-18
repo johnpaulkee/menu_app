a0ef01908ab0b34560ed7c3f6eadf7c425988de2
Alex Land <aland@sfu.ca>
2015-10-18 04:26:35 -0700

    Sorted menu items and prettified 'em

--- www/js/controller.js
+++ www/js/controller.js
@@ -11,20 +11,35 @@ app.controller('MenuController', function($scope, $state, $location) {
 			itemName: menuItem.itemName,
 			itemScore: menuItem.itemScore
 		};
-		console.log(menuItem.itemScore);
+		console.log(updatedItem);
 		ref.child(restaurantName).child("menuItems").child(menuItem.itemName).set(updatedItem);
 	}
 
 	console.log($state.params.name);
 
-	ref.once('value', function(snapshot) {
+	ref.on('value', function(snapshot) {
 			var results = snapshot.exportVal();
 			console.log(results[$state.params.name]);
 			if (results[$state.params.name]) {
-				var menuItems = results[$state.params.name].menuItems;
 				var restaurantName = results[$state.params.name].restaurantName;
-				$scope.menuItems = menuItems;
+				var menuItems = results[$state.params.name].menuItems;
+				var sortedMenu = [];
+				for (var item in menuItems) {
+				    sortedMenu.push([menuItems[item].itemName, menuItems[item].itemScore]);
+				}
+			    console.log(sortedMenu.length);
+				sortedMenu.sort(function(a, b) {return b[1] - a[1]});
+				var sortedMenuItems = {};
+				for (var i = 0; i < sortedMenu.length; i++) {
+					sortedMenuItems[sortedMenu[i][0]] = {
+						itemName: sortedMenu[i][0],
+						itemScore: sortedMenu[i][1]
+					}
+				}
+				console.log(menuItems);
+
 				$scope.restaurantName = restaurantName;
+				$scope.menuItems = sortedMenuItems;
 				$scope.$apply();
 			}
 	});

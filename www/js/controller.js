96e289026d5d483be7ff8a43cf0e9ec2aa0df0e3
John Paul Kee <johnpaulkee@gmail.com>
2015-10-17 22:53:50 -0700

    Added in map details

---
+++ www/js/controller.js
@@ -0,0 +1,69 @@
+angular.module('starter.controllers', ['ionic','firebase'])
+
+.controller('MapCtrl', ['$scope', '$firebase', '$ionicPopup', function($scope, $firebase, $ionicPopup) {
+	$scope.user = {};
+	var firebaseObj = new Firebase("https://crackling-torch-9931.firebaseio.com//MapDetails");
+	var fb = $firebase(firebaseObj);
+
+	$scope.saveDetails = function() {
+		var lat = $scope.user.latitude;
+		var lgt = $scope.user.longitude;
+		var des = $scope.user.desc;
+
+    // Code to write to Firebase will be here
+    fb.$push({
+    	latitude: lat,
+    	longitude: lgt,
+    	description: des
+    }).then(function(ref) {
+    	$scope.user = {};
+    	$scope.showAlert();
+    }, function(error) {
+    	console.log("Error:", error);
+    });
+}
+
+$scope.showAlert = function() {
+	$ionicPopup.alert({
+		title: 'iMapApp',
+		template: 'Your location has been saved!!'
+	});
+};
+
+}])
+
+.directive('map', function() {
+	return {
+		restrict: 'A',
+		link:function(scope, element, attrs){
+
+			var zValue = scope.$eval(attrs.zoom);
+			var lat = scope.$eval(attrs.lat);
+			var lng = scope.$eval(attrs.lng);
+
+
+			var myLatlng = new google.maps.LatLng(lat,lng),
+			mapOptions = {
+				zoom: zValue,
+				center: myLatlng
+			},
+			map = new google.maps.Map(element[0],mapOptions),
+			marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map,
+				draggable:true
+			});
+
+			google.maps.event.addListener(marker, 'dragend', function(evt) {
+				console.log('Current Latitude:',evt.latLng.lat(),'Current Longitude:',evt.latLng.lng());
+			});
+
+			google.maps.event.addListener(marker, 'dragend', function(evt){
+				scope.$parent.user.latitude = evt.latLng.lat();
+				scope.$parent.user.longitude = evt.latLng.lng();
+				scope.$apply();
+			});
+
+		}
+	};
+});

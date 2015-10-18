b94b5563a9015d3eabab2d38eed5eab7dcef5c2a
TimAdamson21 <timadamson21@yahoo.com>
2015-10-18 07:56:25 -0700

    adding map functionality

--- www/js/map_controller.js
+++ www/js/map_controller.js
@@ -51,6 +51,73 @@ app.directive('map', function() {
 						position: initialLocation,
 						map: map
 					});
+					var service = new google.maps.places.PlacesService(map);
+						service.nearbySearch({
+						location: initialLocation,
+						radius: 500,
+						types: ['restaurant']
+					}, processResults);
+					
+						function processResults(results, status, pagination) {
+						  if (status !== google.maps.places.PlacesServiceStatus.OK) {
+							return;
+						  } else {
+							createMarkers(results);
+
+							if (pagination.hasNextPage) {
+							  var moreButton = document.getElementById('more');
+							if(moreButton){
+							  moreButton.disabled = false;
+
+							  moreButton.addEventListener('click', function() {
+								moreButton.disabled = true;
+								pagination.nextPage();
+							  });
+							  }
+							}
+						  }
+						}
+						
+						function createMarkers(places) {
+					  var bounds = new google.maps.LatLngBounds();
+					  var placesList = document.getElementById('places');
+
+					  for (var i = 0, place; place = places[i]; i++) {
+						var image = {
+						  url: place.icon,
+						  size: new google.maps.Size(71, 71),
+						  origin: new google.maps.Point(0, 0),
+						  anchor: new google.maps.Point(17, 34),
+						  scaledSize: new google.maps.Size(25, 25)
+						};
+
+						var marker = new google.maps.Marker({
+						  map: map,
+						  icon: image,
+						  title: place.name,
+						  position: place.geometry.location
+						});
+
+						marker.addListener('click', function() {
+						 window.location.href = "testHTML.html";  
+					  });
+						if(placesList){
+						placesList.innerHTML += '<li id="placeTab">' + place.name + '</li>';
+						}
+
+						bounds.extend(place.geometry.location);
+					  }
+					  map.fitBounds(bounds);
+					  
+					  if(document.getElementById('placeTab')){
+						google.maps.event.addDomListener(document.getElementById('placeTab'), 'click', function(element) {
+						window.alert('Map was clicked!');
+						});
+						}
+					}
+						
+					
+					
 				}, function() {
 					handleNoGeolocation(true);
 				});
@@ -68,6 +135,9 @@ app.directive('map', function() {
 				}
 			},
 			map = new google.maps.Map(element[0],mapOptions);
-		}
+			
+
+			}
+		
 	};
 });
